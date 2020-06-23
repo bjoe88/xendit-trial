9d2e242f0fbb140ebb935a3f2a34e1f83b0d4fc6
Muhammad Azizol Aminuddin <bjoe88@gmail.com>
2020-06-23 13:35:43 +0800

    Add get notification type endpoint (#3)

---
+++ src/model/notificationType.ts
@@ -0,0 +1,71 @@
+
+import uuid = require('uuid');
+import AWS = require('aws-sdk');
+
+AWS.config.setPromisesDependency(require('bluebird'));
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const TABLE_NAME = "notification-table";
+export interface Notification {
+    pk: string
+    sk: string
+    name: string
+    dummyPayload: string
+    "gsi-pk1": string
+    "gsi-sk1": string
+}
+
+export interface NotificationDisplay {
+    id: string
+    name: string
+    dummyPayload: string
+}
+
+export async function createNotificationType(name: string, dummyPayload: string) {
+    const notification = notificationData(name, dummyPayload);
+    const notificationInfo = {
+        TableName: TABLE_NAME,
+        Item: notification,
+    };
+    return dynamoDb.put(notificationInfo).promise()
+        .then(res => toDisplay(notification));
+};
+
+export async function getNotificationTypes() {
+    var params = {
+        TableName: TABLE_NAME,
+        IndexName: "gsi1",
+        KeyConditionExpression: "#gsipk=:pk",
+        ExpressionAttributeValues: {
+            ":pk": "notification-type"
+        },
+        ExpressionAttributeNames: {
+            "#gsipk": "gsi-pk1"
+        }
+    }
+    let result = await dynamoDb.query(params).promise();
+    console.log(result);
+    return result.Items?.map((ele) => {
+        return toDisplay(ele as Notification);
+    })
+};
+
+const notificationData = (name: string, dummyPayload: string): Notification => {
+    const timestamp = new Date().getTime();
+    return {
+        pk: uuid.v1(),
+        sk: timestamp.toString(),
+        name: name,
+        dummyPayload: dummyPayload,
+        "gsi-pk1": "notification-type",
+        "gsi-sk1": timestamp.toString()
+    };
+};
+
+const toDisplay = (data: Notification): NotificationDisplay => {
+    return {
+        id: data.pk,
+        name: data.name,
+        dummyPayload: data.dummyPayload
+    }
+}
